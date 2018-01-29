da79cac8d1c953784a9888e60250e549615e179f
Choi Yein <yeiny1004@naver.com>
2018-01-29 10:36:10 +0900

    안전하지 않은 스크립트 수정

--- js/jsapi.js
+++ js/jsapi.js
@@ -4,7 +4,7 @@ window['google'] = {};
 if (!window['google']['loader']) {
 window['google']['loader'] = {};
 google.loader.ServiceBase = 'http://www.google.com/uds';
-google.loader.GoogleApisBase = 'http://ajax.googleapis.com/ajax';
+google.loader.GoogleApisBase = 'https://ajax.googleapis.com/ajax';
 google.loader.ApiKey = 'notsupplied';
 google.loader.KeyVerified = true;
 google.loader.LoadFailure = false;
